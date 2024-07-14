01f5ff5d197a76c58e51e250bfea4d22d57a8c88
Aneth-2 <aquispeh@unjbg.edu.pe>
2024-07-14 17:16:27 -0500

    Añade registros1.cpp

---
+++ registros1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <limits> 
+
+using namespace std;
+
+struct Empleado {
+    string nombre;
+    string sexo;
+    int sueldo;
+};
+
+int main() {
+    int n;
+    
+    cout << "Ingrese cantidad de empleados: ";
+    cin >> n;
+
+
+    return 0;
+}
+
