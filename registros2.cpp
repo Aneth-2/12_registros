81197eea6beac8a40aad105ec3c11899dfa7d3bd
Aneth-2 <aquispeh@unjbg.edu.pe>
2024-07-14 18:48:11 -0500

    Añade registros2.cpp

---
+++ registros2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <iomanip> 
+
+using namespace std;
+
+struct Persona {
+    string nombres;
+    string dni;
+    int edad;
+};
+
+int main() {
+    int n;
+
+    cout << "Ingrese la cantidad de personas: ";
+    cin >> n;
+
+
+    return 0;
+}
+
