14f349e172247cc65823976e6814925e4bc6ee77
Aneth-2 <aquispeh@unjbg.edu.pe>
2024-07-14 19:10:16 -0500

    se agrego el void para pedir los datos de las personas

--- registros3.cpp
+++ registros3.cpp
@@ -10,6 +10,19 @@ struct Persona {
     int anioNacimiento;
 };
 
+void pedirDatos(Persona* personas, int n) {
+    for (int i = 0; i < n; i++) {
+        cout << "Ingrese el nombre de la persona " << i + 1 << ": ";
+        cin >> personas[i].nombre;
+        cout << "Ingrese el dia de nacimiento de " << personas[i].nombre << ": ";
+        cin >> personas[i].diaNacimiento;
+        cout << "Ingrese el mes de nacimiento de " << personas[i].nombre << ": ";
+        cin >> personas[i].mesNacimiento;
+        cout << "Ingrese el anio de nacimiento de " << personas[i].nombre << ": ";
+        cin >> personas[i].anioNacimiento;
+    }
+}
+
 
     return 0;
 }
