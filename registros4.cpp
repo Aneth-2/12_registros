8e3973ce1d083273709012eec31e879e3c60ec41
Aneth-2 <aquispeh@unjbg.edu.pe>
2024-07-14 19:27:31 -0500

    se añadio registros4.cpp

---
+++ registros4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Atleta {
+    string nombre;
+    string pais;
+    string disciplina;
+    int numMedallas;
+};
+
+
+    return 0;
+}
+
